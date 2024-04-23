e4da62c23dc0e535bd92de790792db6dd5c11243
juanesgv <juanesgarviedma23@gmail.com>
2024-04-22 23:26:29 -0500

    creación del componente modal y sus respectivos actions para abrir y cerrar el modal

---
+++ src/components/ExpenseForm.tsx
@@ -0,0 +1,10 @@
+
+const ExpenseForm = () => {
+    return (
+        <div>
+            ExpenseForm
+        </div>
+    )
+}
+
+export default ExpenseForm
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,12 +1,16 @@
 export type BudgetActions = 
-    {type: 'add-budget', payload: {budget:number}}
+    {type: 'add-budget', payload: {budget:number}} |
+    {type: 'show-modal'} |
+    {type: 'close-modal'} 
 
 export type BudgetState = {
-    budget: number
+    budget: number,
+    modal: boolean
 }
 
 export const initialState : BudgetState = {
-    budget: 0
+    budget: 0,
+    modal: false
 }
 
 export const budgetReducer = (
@@ -15,10 +19,24 @@ export const budgetReducer = (
 ) => {
 
     if(action.type === 'add-budget'){
-            return {
-                ...state,
-                budget: action.payload.budget
-            }
+        return {
+            ...state,
+            budget: action.payload.budget
+        }
+    }
+
+    if(action.type === 'show-modal'){
+        return{
+            ...state,
+            modal: true
+        }
+    }
+
+    if(action.type === 'close-modal'){
+        return{
+            ...state,
+            modal: false
+        }
     }
     
     return state
