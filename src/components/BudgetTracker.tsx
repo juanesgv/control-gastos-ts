37fc485e26c6903647e6da216f2db563888ed2e6
juanesgv <juanesgarviedma23@gmail.com>
2024-05-27 22:31:50 -0500

    mostrar los valores de presupuesto disponible y gastado

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,6 +1,15 @@
+import { useMemo } from "react"
+import { useBudget } from "../hooks/useBudget"
 import AmountDisplay from "./AmountDisplay"
 
 const BudgetTracker = () => {
+
+    const {state} = useBudget()
+
+    const totalExpenses = useMemo(()=> state.expenses.reduce((total, expense)=> expense.amount + total, 0) , [state.expenses])
+
+    const remainingBudget = state.budget - totalExpenses
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             <div className="flex justify-center">
@@ -17,15 +26,15 @@ const BudgetTracker = () => {
 
                 <AmountDisplay
                     label="Presupuesto"
-                    amount={300}
+                    amount={state.budget}
                 />
                 <AmountDisplay
                     label="Disponible"
-                    amount={200}
+                    amount={remainingBudget}
                 />
                 <AmountDisplay
                     label="Gastado"
-                    amount={100}
+                    amount={totalExpenses}
                 />
             </div>
         </div>
