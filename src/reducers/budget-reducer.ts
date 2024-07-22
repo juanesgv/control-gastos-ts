6680e544f1c21b85cd72429e9a1d3e4d65a4e93e
juanesgv <juanesgarviedma23@gmail.com>
2024-07-21 22:50:28 -0500

    creación del componente para filtrar gastos

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,4 +1,4 @@
-import { DraftExpense, Expense } from "../interfaces"
+import { Category, DraftExpense, Expense } from "../interfaces"
 import { v4 as uuidv4 } from 'uuid'
 
 export type BudgetActions =
@@ -8,14 +8,16 @@ export type BudgetActions =
     { type: 'add-expense', payload: { expense: DraftExpense } } |
     { type: 'remove-expense', payload: { id: Expense['id'] } } |
     { type: 'get-expense-by-id', payload: { id: Expense['id'] } } |
-    { type: 'update-expense', payload: { expense: Expense } } |
-    { type: 'reset-app' }
+    { type: 'update-expense', payload: { expense: Expense } } | 
+    { type: 'reset-app' } |
+    { type: 'add-filter-category', payload: { id: Category['id'] } } 
 
 export type BudgetState = {
     budget: number,
     modal: boolean,
     expenses: Expense[],
     editingId: Expense['id']
+    currentCategory: Category['id']
 }
 
 
@@ -34,7 +36,8 @@ export const initialState: BudgetState = {
     budget: initialBudget(),
     modal: false,
     expenses: localStorageExpense(),
-    editingId: ''
+    editingId: '',
+    currentCategory: ''
 }
 
 const createExpense = (draftExpense: DraftExpense): Expense => {
@@ -110,6 +113,13 @@ export const budgetReducer = (
             ...state,
             budget: 0,
             expenses: [],
+        }
+    }
+
+    if (action.type === 'add-filter-category'){
+        return {
+            ...state,
+            currentCategory: action.payload.id
         }
     }
 
